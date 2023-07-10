eb59f0af4d338bd42d2bc10bc9851dd296ab9c73
328296826261 <skevalku@amazon.com>
2023-07-10 11:35:12 +0100

    changing backend and changing region

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
   backend "s3" {
     bucket         = "personio-kubernetes-challenge-keval-tf-state-backend-ci-cd"
     key            = "tf-infra/terraform.tfstate"
-    region         = "eu-west-1"
+    region         = "us-east-2"
     dynamodb_table = "terraform-personio-tf-state-locking"
     encrypt        = true
   }
